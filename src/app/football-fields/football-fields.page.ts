1dbcda61b741a0d82ed2e148b0c04b45bba92457
Ahmad Samim Qeyam <qeyam_a@auca.kg>
2020-04-26 19:44:35 +0600

    Searchbar added in Football Fields and Sport Clubs

--- src/app/football-fields/football-fields.page.ts
+++ src/app/football-fields/football-fields.page.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { FootballFieldsService } from './football-fields.service';
 import { FootballField } from './football-fields.model';
 import { Subscription } from 'rxjs';
+import { IonSearchbar } from '@ionic/angular';
+
 @Component({
   selector: 'app-football-fields',
   templateUrl: './football-fields.page.html',
   styleUrls: ['./football-fields.page.scss'],
 })
 export class FootballFieldsPage implements OnInit, OnDestroy {
+  @ViewChild('searchInput', {static: false}) searchInput: IonSearchbar;
+
   loadedFootballFields: FootballField[];
   isLoading = false;
   private fieldsSub: Subscription;
@@ -20,7 +24,20 @@ export class FootballFieldsPage implements OnInit, OnDestroy {
     });
   }
 
+  searchField(event: any) {
+    this.fieldsSub = this.footballFieldService.footballFields.subscribe(footballField => {
+      this.loadedFootballFields = footballField;
+    });
+    const val = event.target.value;
+    if (val && val.trim() !== '') {
+      this.loadedFootballFields = this.loadedFootballFields.filter(field => {
+        return (field.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    }
+  }
+
   ionViewWillEnter() {
+    this.searchInput.value = '';
     this.isLoading = true;
     this.footballFieldService.fetchFields().subscribe(() => {
       this.isLoading = false;
--- src/app/other-sport-clubs/other-sport-clubs.page.ts
+++ src/app/other-sport-clubs/other-sport-clubs.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { SportClubs } from './sport-clubs.model';
 import { OtherSportClubsService } from './other-sport-clubs.service';
 import { Subscription } from 'rxjs';
+import { IonSearchbar } from '@ionic/angular';
 
 @Component({
   selector: 'app-other-sport-clubs',
@@ -9,11 +10,24 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./other-sport-clubs.page.scss'],
 })
 export class OtherSportClubsPage implements OnInit, OnDestroy {
+  @ViewChild('searchInput', {static: false}) searchInput: IonSearchbar;
   loadedSportClubs: SportClubs[];
   private clubsSub: Subscription;
   isLoading = false;
 
   constructor(private otherSportClubsService: OtherSportClubsService) { }
+
+  searchClub(event: any) {
+    this.clubsSub = this.otherSportClubsService.sportClubs.subscribe(sportClubs => {
+      this.loadedSportClubs = sportClubs;
+    });
+    const val = event.target.value;
+    if (val && val.trim() !== '') {
+      this.loadedSportClubs = this.loadedSportClubs.filter(club => {
+        return (club.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    }
+  }
 
   ngOnInit() {
     this.clubsSub = this.otherSportClubsService.sportClubs.subscribe(sportClubs => {
@@ -22,6 +36,7 @@ export class OtherSportClubsPage implements OnInit, OnDestroy {
   }
 
   ionViewWillEnter() {
+    this.searchInput.value = '';
     this.isLoading = true;
     this.otherSportClubsService.fetchSportClubs().subscribe(() => {
       this.isLoading = false;
